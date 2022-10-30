07979c622c3e625b742fea0141e0c46321bc2ae6
muhammed-swalih <swalihnaduthodi4@gmail.com>
2022-10-30 13:54:19 +0530

    initial commit

--- src/components/Protofolio.jsx
+++ src/components/Protofolio.jsx
@@ -13,11 +13,11 @@ function Protofolio() {
       <div>
         <h1 className='text-4xl sm:pl-14'>Protofolio</h1>
       </div>
-      <div className='leading-9 py-8 sm:pl-14 md:max-w-3xl lg:max-w-5xl'>
+      <div className='leading-9  py-8 sm:pl-14 md:max-w-3xl lg:max-w-5xl'>
           <p>Since the beginning of my journey as a freelance designer and developer, I've done remote work for agencies consulted for startups and collaborated with talanted people to create digital products for both business and consumer use.</p>
           <p className='pt-4'>I offer from a wide range of services, including brand design, programming and teaching.</p>
       </div>
-      <div className='py-5 grid grid-rows-6 gap-7 md:flex flex-row flex-wrap lg:mx-auto p-14'>
+      <div className='py-5 grid grid-rows-6 gap-7 md:flex flex-row flex-wrap lg:mx-auto'>
         <div className='text-center w-80 shadow-lg peer-last-of-type:-10 sm:mx-auto basis-1/3 flex-1 '>
           {
             <img src={web1} alt=""  className='rounded-lg object-cover'  />
